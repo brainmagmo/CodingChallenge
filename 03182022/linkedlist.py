39ea4ef89eeadf36da394e4701b6ad202f8e3903
Trevor John Kleinstuber <43195637+brainmagmo@users.noreply.github.com>
2022-03-15 12:44:14 -0700

    Create linkedlist.py

---
+++ 03182022/linkedlist.py
@@ -0,0 +1,11 @@
+#@Author: Trevor Kleinstuber
+#@Last Modified: 15 March 2022
+class Node
+  def __init__(self, value):
+    self.value = value
+    self.next = None
+
+class SLinkedList:
+   def __init__(self):
+      self.head = None
+
