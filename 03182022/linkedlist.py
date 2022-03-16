8fd854bfaee13ba4e1da744130c24a4ec3832dcf
Trevor John Kleinstuber <trevor.kleinstuber@gmail.com>
2022-03-15 17:46:43 -0700

    created a linked list class and implemented a solution to the coding challenge

--- 03182022/linkedlist.py
+++ 03182022/linkedlist.py
@@ -1,11 +1,176 @@
 #@Author: Trevor Kleinstuber
 #@Last Modified: 15 March 2022
-class Node
-  def __init__(self, value):
+
+class Node:
+  def __init__(self, value=None):
     self.value = value
     self.next = None
 
-class SLinkedList:
-   def __init__(self):
-      self.head = None
+  #assuming nodes with same value are the same node
+  def equals(self, other):
+    return self == other or (isinstance(other, Node) and self.value == other.value)
 
+
+class SLinkedList:
+  def __init__(self):
+    self.head = None
+
+  def push(self, value):
+    temp = self.head
+    self.head = value if isinstance(value, Node) else Node(value)
+    self.head.next = temp
+    return self
+
+  def pop(self):
+    temp = self.head
+    if temp != None:
+      self.head = temp.next
+      temp.next = None
+    return temp
+
+  def get_tail(self):
+    tail = self.head
+    if(not tail):
+      return None
+
+    else:
+      while tail.next:
+        tail = tail.next
+    return tail
+
+  def append(self, value):
+    tail = self.get_tail()
+    new_node = value if isinstance(value, Node) else Node(value)
+    if tail:
+      tail.next = new_node
+    else:
+      self.head = new_node
+    return self
+
+  def add_all(self, arr):
+    tail = self.get_tail()
+    if not tail:
+      if not arr:
+        return self
+
+      else:
+        self.head = arr[0] if isinstance(arr[0], Node) else Node(arr[0])
+        arr = arr[1:]
+        tail = self.head
+    for item in arr:
+      new_node = item if isinstance(item, Node) else Node(item)
+      tail.next = new_node
+      tail = tail.next
+    return self
+
+  def __len__(self):
+    i = 0
+    temp = self.head
+    while temp:
+      i += 1
+      temp = temp.next
+    return i
+    
+  def length(self):
+    return len(self)
+
+  def __iter__(self):
+    temp = self.head
+    while(temp):
+      yield temp
+      temp = temp.next
+
+  def __getitem__(self, index):
+    if not isinstance(index, int):
+      raise TypeError('index not an Integer')
+
+    if index < 0:
+      index = len(self) + index
+      if index < 0:
+        raise IndexError
+
+    temp = self.head
+    if temp:
+      while index and temp.next:
+        index  -= 1
+        temp = temp.next
+      if index and not temp.next:
+        raise IndexError
+
+    return temp
+    
+  def get_at(self, index):
+    return self[index]
+
+  def insert_at(self, value, index):
+    new_node = value if isinstance(value, Node) else Node(value)
+    l = self.len()
+    if index == 0 or index == -l:
+      return self.push(value)
+
+    else:
+      temp = self.get_at(index - 1)
+      tempnext = temp.next
+      temp.next = new_node
+      new_node.next = tempnext
+      return self
+
+  def remove_at(self, index):    
+    l = self.len()
+    if index == 0 or index == -l:
+      return self.pop()
+
+    else:
+      prev = self.get_at(index - 1)
+      temp = prev.next
+      prev.next = temp.next
+      temp.next = None
+      return temp
+
+  def to_s(self):
+    string = "Singly linked list:\n"
+    if self.head:
+      temp = self.head
+      i = 0
+      while temp.next:
+        string += "Value of node at index" + str(i) + ": " + str(temp.value) + "\n"
+        temp = temp.next
+        i += 1
+      string += "Value of node at index" + str(i) + ": " + str(temp.value) +"\n"
+    else:
+        string = "Empty list"
+    return string
+
+def intersection(list1, list2):
+  delta = list1.len() - list2.len()
+  current1 = list1.head
+  curren2 = list2.head
+  while(delta>0):
+    current1 = current1.next
+    delta -= 1
+  while(delta<0):
+    curren2 = curren2.next
+    delta += 1
+  while(not current1.equals(curren2)):
+    current1 = current1.next
+    curren2 = curren2.next
+  return current1
+
+# def find_intesection(l1, l2):
+#   # if(not(isinstance(l1, SLinkedList) and isinstance(l2, SLinkedList))):
+#   #   raise TypeError
+#   this = l1.head
+#   while(this != None):
+#     this.checked = True
+#     this = this.next
+#   that = l2.head
+#   while(not that.checked):
+#     that = that.next
+#   r = None
+#   if(that.checked):
+#      r = that
+#   this = l1.head
+#   while(this != None):
+#     this.checked = False
+#     this = this.next
+#   return r
---
+++ 03182022/test.py
@@ -0,0 +1,71 @@
+from linkedlist import Node, SLinkedList, intersection
+
+j = Node()
+
+ending = SLinkedList()
+ending.add_all(['a',50,Node('c')])
+left = SLinkedList()
+left.add_all([50.5,'foo',{'key':'value'}])
+right = SLinkedList()
+right.add_all(['virginia',[1,2,3],'marco','polo','green',1000,100000.5,SLinkedList.__dict__,'z'])
+#print(right.at_index(-2).value)
+right.get_tail().next = ending.head
+left.get_tail().next = ending.head.next
+print("expected: 50\nactual:")
+print(intersection(left,right).value)
+
+farleft = SLinkedList()
+farleft.push(1).add_all([2,3])
+farleft.get_at(-1).next = left.get_at(-2)
+
+centrist = SLinkedList()
+centrist.push(left.get_at(-1))
+centrist.push('talkingpoints')
+print("expected: c\nactual:")
+print(intersection(farleft,centrist).value)
+
+if([]):
+  print("truthy")
+else:
+  print("falsy")
+
+n1 = Node(5)
+n2 = Node(5)
+n3 = Node(6)
+n4 = n3
+n5 = Node(7)
+n6 = Node(8)
+n1.next = n3
+n2.next = n3
+print("test1")
+print(n1 == n2)
+print("test1b")
+print(n1.equals(n2))
+print("test2")
+print(n3 == n4)
+n3.next = n5
+n4.next = n6
+print("test3")
+print(n3 == n4)
+print("test4")
+print(n3.next)
+
+L = SLinkedList()
+L.push(5).push(200).push(77)
+print(L.to_s())
+L.pop()
+print(L.to_s())
+print("\nlength\n")
+print(L.len())
+
+M = SLinkedList()
+M.add_all([5,50,500,5000])
+print(M.to_s())
+print(M.get_tail().value)
+print(M.get_at(0).value)
+print(M.get_at(2).next.value)
+print(M.get_at(-1).value)
+try:
+  print(M.get_at(10))
+except IndexError as e:
+  print("error fired succesfully")
